fd2395b8d7cba6bd999854f4cee8de34f3b2bf70
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-13 11:04:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,13 @@ st.subheader("Is your job worth keeping? Should you stay? Or just leave? Let's t
 st.write("You can see below for more information")
 
 # Load dataset (Ensure the CSV file is in the correct location)
-data = pd.read_csv("https://raw.githubusercontent.com/ibniinggrianti/attritionprediction/refs/heads/master/IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/ibniinggrianti/attritionprediction/refs/heads/master/IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv")
+df
 
+X = df.drop('Attrition', axis=1)
+
+y = df.Attrition
+  
 with st.sidebar:
   st.header('Input Features')
   Age = st.slider("Age", min_value=18, max_value=60, value=25)
