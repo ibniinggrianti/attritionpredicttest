1b7d99262f584dc1537d13229c072fe7972d348b
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-13 16:08:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 #st.title('Attrition Prediction')
