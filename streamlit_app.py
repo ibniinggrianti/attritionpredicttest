778b0d27ce8ed6785e97c8b61828b40cb7bb7183
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-13 15:15:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,19 +73,8 @@ with st.expander('Data Preparation'):
   st.write('**Encoded y**')
   st.dataframe(y)
 
-print(X.dtypes)
-print(type(y))
-print(X.shape, y.shape)
-
-#Model Training
-clf = RandomForestClassifier()
-clf.fit(X, y)
-
-## Apply model to make predictions
-prediction = clf.predict(input_row)
-prediction_proba = clf.predict_proba(input_row)
-
-df_prediction_proba 
+# Train-Test Split
+X_train, X_test, y_train, y_test = train_test_split(X_raw, y_raw, test_size=0.2, random_state=42)
 
 
 
