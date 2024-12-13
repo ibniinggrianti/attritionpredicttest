50bdf766ab7e4f6afb8c9d748986670f03f4e33c
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-13 10:43:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.title('🎈 App Name')
+st.title('Attrition Prediction')
 
-st.write('Hello world!')
+st.subheader("Is your job worth keeping? Should you stay? Or just leave? Let's try!")
+st.write("You can see below for more information")
+
+# Load dataset (Ensure the CSV file is in the correct location)
+data = pd.read_csv("https://raw.githubusercontent.com/ibniinggrianti/attritionprediction/refs/heads/master/IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv")
+
+with st.sidebar:
+  st.header('Input Features')
+  Age = st.slider("Age", min_value=18, max_value=60, value=25)
+  st.write(f"Your selected option: {Age}.")
